a6b868bbd629c5dfe2e4806a98b49ff13199317d
KD2-87243-Kartik <kartikjachak16@gmail.com>
2024-12-26 16:43:37 +0530

    Assignment 1 Complete

---
+++ Assignments/Assignment_1/Assignment_1_Q2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Menu----------");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Substraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            
+                switch (choice) 
+                {
+                    case 1: 
+                        Console.WriteLine("Addition: " + (num1 + num2)); 
+                    break;
+
+                    case 2:
+                        Console.WriteLine("Substraction: " + (num1 - num2));
+                    break;
+
+                    case 3:
+                        Console.WriteLine("Multiplication: " + (num1 * num2));
+                    break;
+
+                    case 4:
+                        Console.WriteLine("Division: " + (num1 / num2));
+                    break;
+
+                }
+        }
+    }                          
+}                              
---
+++ Assignments/Assignment_1/Assignment_1_Q3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Console.WriteLine("Enter two numbers: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+
+            do
+            {
+                Console.WriteLine("Menu----------");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Substraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Exit");
+                
+                Console.WriteLine("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Addition: " + (num1 + num2));
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Substraction: " + (num1 - num2));
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Multiplication: " + (num1 * num2));
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Division: " + (num1 / num2));
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                }
+
+            } while (choice != 5);
+        }
+    }
+}
